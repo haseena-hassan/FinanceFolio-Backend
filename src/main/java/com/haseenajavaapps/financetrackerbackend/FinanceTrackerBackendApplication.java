bc6107727175f218e987b517740681cb2f2bf3b5
haseena-hassan <haseena2199@gmail.com>
2024-05-02 16:53:08 +0530

    spring app setup

---
+++ src/main/java/com/haseenajavaapps/financetrackerbackend/FinanceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.haseenajavaapps.financetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haseenajavaapps/financetrackerbackend/FinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haseenajavaapps.financetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
